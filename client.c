22934c8b11cd46f9552d651522c9d85cf79c835b
rfenzo <rfenzo@uc.cl>
2018-06-08 03:06:21 -0400

    voy en opponents found, no esta fuerdando los nombres es "nicknames"

--- client.c
+++ client.c
@@ -2,42 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <poll.h>
+#include <unistd.h>
+#include <stdbool.h>
 
+struct card{
+  int pinta;
+  int numero;
+	bool valid;
+};
+typedef struct card Card;
 
 int initializeClient(char* ip, int port){
-	int clientSocket;
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
 
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  int clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(port);
   serverAddr.sin_addr.s_addr = inet_addr(ip);
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
-	// printf("Connected to server!\n");
-  /*---- Read the buffer from the server into the buffer ----*/
-  // recv(clientSocket, buffer, 1024, 0);
-  // /*---- Print the received buffer ----*/
+  addr_size = sizeof serverAddr;
+	int x = connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+	if (x<0) {
+		printf("ERROR: %s\n", strerror(errno));
+		return -1;
+	}
+  /*---- Read the message from the server into the buffer ----*/
+  // recv(clientSocket, buffer, 256, 0);
+  // /*---- Print the received message ----*/
   // printf("Data received: %s",buffer);
   // strcpy(buffer,"Bye World\n");
   // send(clientSocket,buffer,11,0);
 	return clientSocket;
 }
 
-char* recieveMessage(int socket, char* buffer){
-  // printf("Waiting buffer... ♔ \n");
-  recv(socket, buffer, 256, 0);
-  return buffer;
+void sendMessage(int socket, char* message){
+  send(socket, message, 256,0);
 }
 
-void sendMessage(int socket, char* buffer){
-  send(socket, buffer, 256,0);
-}
-
-char* readBuffer(char* buffer,unsigned int* id){
+char* readBuffer(char* buffer, int* id){
   *id = (int) (buffer[0] -'0');
   unsigned int payloadSize = (int)(buffer[1]-'0');
   char* payload = malloc(payloadSize);
@@ -48,34 +55,114 @@ char* readBuffer(char* buffer,unsigned int* id){
 }
 
 int main(int argc, char const *argv[]) {
-	//WARNING falta hacer que los argumentos puedan ser pasados en desorden
-	//WARNING falta hacer que no se caiga al recibir malos paquetes
-
   if (argc != 3) {
-    printf("Argumentos inadecuados\n$ ./client -i <ip_address> -p <tcp-port>\n");
+    printf("Número de argumentos inadecuado\n$ ./client -i <ip_address> -p <tcp-port>\n");
     return 1;
   }
-
   int socket = initializeClient((char*)argv[1], atoi(argv[2]));
-
-	// char* buffer = malloc(sizeof(char)*256);
-	char buffer[256];
-	unsigned int id;
-	sendMessage(socket, "100");
-	recieveMessage(socket, buffer);
-	char* payload = readBuffer(buffer, &id);
-	if (id == 2) {
-		printf("Solicitud de conexion aceptada!\n");
+	if (socket < 0) {
+		return 2;
 	}
-	// printf(msg, "%s\n");
-	//
+
+	struct pollfd fds[1];
+	fds[0].fd = socket;
+	fds[0].events = POLLIN;
+
+	int timeout_msecs = 1000;
+	int ret, readedBytes, id, pot, i;
+	char buffer[256];
+	char contrincante[254];
+	Card* hand[5];
+
+	printf("Solicitando participar en el juego\n");
+	sendMessage(socket, "100");
+
+	while(1){
+		ret = poll(fds, 1, timeout_msecs);
+		if (ret > 0) {
+			readedBytes = read(fds[0].fd, buffer, 256);
+			if (readedBytes == 0) {
+				// WARNING que pasa acá?.
+			}else{
+				// waitingResponse = false;
+				char* payload = readBuffer(buffer, &id);
+				if (id == 2) {
+					//Connection Established
+					printf("Solicitud aceptada!\n");
+				}else if (id == 3) {
+					//Ask Nickname
+					  printf("Ingresa tu nombre de usuario: ");
+						char nickname[254];
+					  scanf("%s", nickname);
+						buffer[0] = '4';
+						buffer[1] = strlen(nickname)+'0';
+						buffer[2] = 0; // hace que se concatene desde buffer[2]
+						strcat(buffer,nickname);
+						sendMessage(socket, buffer);
+				}else if (id == 5) {
+					//Opponent Found
+					*contrincante = *payload;
+					printf("Contrincante: %s\n", contrincante);
+				}else if (id == 6) {
+					//Initial Pot
+					pot = (int) payload;
+				}else if (id == 7) {
+					//Game Start
+				}else if (id == 8) {
+					//Start Round
+					pot = (int) payload;
+				}else if (id == 9) {
+					//Initial Bet
+					pot -= 10;
+				}else if (id == 10) {
+					//5-Cards
+					printf("getting cards\n");
+					for (i = 0; i < 5; i++) {
+						hand[i]->numero = (int)payload[2*i];
+						hand[i]->pinta = (int)payload[2*i+1];
+						hand[i]->valid = true;
+						printf("%i: numero %i, pinta %i\n",i, hand[i]->numero, hand[i]->pinta);
+					}
+				}else if (id == 11) {
+					//Who's First
+				}else if (id == 12) {
+					//Get Cards to Change
+				}else if (id == 14) {
+					//Get Bet
+				}else if (id == 15) {
+					//Return Bet
+				}else if (id == 16) {
+					//Error Bet
+				}else if (id == 17) {
+					//Ok Bet
+				}else if (id == 18) {
+					//End Round
+				}else if (id == 19) {
+					//Show Opponent Cards
+				}else if (id == 20) {
+					//Winner/Loser
+				}else if (id == 21) {
+					//Update Pot
+				}else if (id == 22) {
+					//Game End
+				}else if (id == 23) {
+					//Image
+				}else if (id == 24) {
+					//Error Not Implemented
+				}
+			}
+		}
+	}
+
+	// char* message = malloc(sizeof(char)*256);
   // while (1) {
   //   printf("\nYour Message: ");
-  //   scanf("%s", buffer);
+  //   scanf("%s", message);
   //   printf("\n");
-  //   sendMessage(socket, buffer);
-  //   char* msg = recieveMessage(socket, buffer);
-  //   printf(msg, "%s\n");
+  //   sendMessage(socket, message);
+  //   // char* msg = recieveMessage(socket, message);
+  //   // printf(msg, "%s\n");
+	// 	// sleep(2);
   // }
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,10 +1,10 @@
-#include <stdlib.h> //atoi
-#include <stdio.h> //printf
-#include <arpa/inet.h> //socket
-#include <stdbool.h> //true, false
-#include <unistd.h> //read
-#include <string.h> //strlen
-#include <time.h> //time
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 struct card{
   int pinta;
@@ -13,89 +13,15 @@ struct card{
 };
 typedef struct card Card;
 
-struct player{
-  int socket;
-  char* nickname;
-  Card* hand[5];
-};
-typedef struct player Player;
-
-Player* getPlayer(int sd, Player** connected){
-  for (int i = 0; i < 2; i++) {
-    if (connected[i]->socket == sd) {
-      return connected[i];
-    }
+Card** handMaker(){
+  Card** hand = malloc(sizeof(Card*)*5);
+  for (int i = 0; i < 5; i++) {
+    hand[i] = malloc(sizeof(Card));
   }
-  return NULL;
+  return hand;
 }
 
-void swap (Card* a, Card* b){
-    Card temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-Card** generateDeckOfCards(Card** deck){
-  // Card** deck = malloc(sizeof(Card)*52);
-  for (int i = 0; i < 13; i++) {
-    for (int j = 0; j < 4; j++) {
-      deck[i+j]->numero = i+1;
-      deck[i+j]->pinta = j+1;
-      deck[i+j]->taken = false;
-    }
-  }
-  //shuffle
-  srand(time(NULL));
-  for (int i = 52-1; i > 0; i--){
-      int j = rand() % (i+1);
-      swap(deck[i], deck[j]);
-  }
-  return deck;
-}
-
-Card* getRandomCard(Card** deck){
-  for (int i = 0; i < 52; i++) {
-    if (!deck[i]->taken) {
-      deck[i]->taken = true;
-      return deck[i];
-    }
-  }
-  return NULL;
-}
-
-bool compareCards(Card* card1, Card* card2){
-  return (card1->numero == card2->numero && card1->pinta == card2->pinta);
-}
-
-void changeCards(Player* currentPlayer, char* payload, Card** deck){
-  Card** oldCards = malloc(sizeof(Card)*(sizeof(payload)/2));
-  for (int i = 0; i < sizeof(payload); i++) {
-    if (i % 2 == 0) {
-      oldCards[i/2]->numero = (int)payload[i];
-    }else{
-      oldCards[i/2]->pinta = (int)payload[i];
-    }
-  }
-  //cambiar las cartas del player con unas nuevas
-  for (int j = 0; j < sizeof(payload)/2; j++) {
-    for (int i = 0; i < 5; i++) {
-      if (compareCards(currentPlayer->hand[i], oldCards[j])) {
-        Card* newCard = getRandomCard(deck);
-        if (newCard) {
-          currentPlayer->hand[i] = newCard;
-        }else{
-          //WARNING que hacer?
-          printf("No quedan cartas en el mazo\n");
-        }
-        break;
-      }
-    }
-  }
-  //quizas se deba retornas las nuevas cartas
-}
-
-// char* readBuffer(char* buffer,unsigned int* id, unsigned int* payloadSize){
-char* readBuffer(char* buffer,unsigned int* id){
+char* readBuffer(char* buffer, int* id){
   *id = (int) (buffer[0] -'0');
   unsigned int payloadSize = (int)(buffer[1]-'0');
   char* payload = malloc(payloadSize);
@@ -106,177 +32,123 @@ char* readBuffer(char* buffer,unsigned int* id){
 }
 
 void sendMessage(int socket, char* message){
-  send(socket, message, sizeof(message),0);
+  send(socket, message, 256,0);
 }
 
-int main(int argc, char const *argv[]) {
-  //WARNING falta hacer que los argumentos puedan ser pasados en desorden
-  //WARNING falta hacer que no se caiga al recibir malos paquetes
+void opponentFound(char** nicknames, struct pollfd* fds){
+  for (int i = 0; i < 2; i++) {
+    printf("%s\n",nicknames[i]);
+    char buffer[254];
+    buffer[0] = '5';
+    buffer[1] = strlen(nicknames[i])+'0';
+    buffer[2] = 0; // hace que se concatene desde buffer[2]
+    strcat(buffer,nicknames[i]);
+    sendMessage(fds[i].fd, buffer);
+  }
+}
 
-  if (argc != 3) {
-		printf("Argumentos inadecuados\n$ ./server -i <ip_address> -p <tcp-port>\n");
+int main (int argc, char *argv[])
+//Elemento 1 de argv será 1 si es server o 0 si es client
+{
+	if (argc != 3) {
+		printf("Número de argumentos inadecuado\n$ ./server -i <ip_address> -p <tcp-port>\n");
 		return 1;
 	}
 
-  const char* IP = argv[1];
-  int PORT = atoi(argv[2]);
-  int MAX_CLIENTS = 30;
+	int welcomeSocket;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size;
 
+	welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(atoi(argv[2]));
+	serverAddr.sin_addr.s_addr = inet_addr((char*)argv[1]);
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-  int clientSockets[MAX_CLIENTS];
-  for (int i = 0; i < MAX_CLIENTS; i++){
-      clientSockets[i] = 0;
-  }
+  if(listen(welcomeSocket,2)!=0)
+    printf("Error\n");
 
-  int masterSocket = socket(AF_INET , SOCK_STREAM , 0);
-  if( masterSocket == 0){
-      printf("Server socket falló en su creación");
-      return 2;
-  }
-  int opt = 1;
-  int reuseaddr = setsockopt(masterSocket,
-                            SOL_SOCKET,
-                            SO_REUSEADDR,
-                            (char *)&opt,
-                            sizeof(opt));
-  if( reuseaddr  < 0 ){
-    printf("Error al setear reuseaddr\n");
-    return 5;
-  }
+  struct pollfd fds[3];
+	fds[0].fd = welcomeSocket;
+  fds[0].events = POLLIN;
+  fds[1].fd = -1;
+  fds[1].events = POLLIN;
+  fds[2].fd = -1;
+  fds[2].events = POLLIN;
 
-  struct sockaddr_in address;
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = inet_addr(IP);
-  address.sin_port = htons( PORT );
+  char* nicknames[2] = {malloc(sizeof(char*)),malloc(sizeof(char*))};
+  Card** hands[2] = {handMaker(),handMaker()};
 
-  int stat = bind(masterSocket, (struct sockaddr*)&address, sizeof(address));
-  if (stat<0){
-      printf("Bind del server socket falló");
-      return 3;
-  }
-  printf("Servidor escuchando el puerto %d\n", PORT);
-
-  if (listen(masterSocket, 3) < 0){
-      printf("Listen del server socket falló");
-      return 4;
-  }
-
-  int addressLength = sizeof(address);
-  unsigned int max_sd, sd, i, actividad, new_socket, readedBytes, id;
-  // unsigned int  payloadSize;
-  char buffer[256];
-  fd_set readfds;
-
-  //almacena el socket de los usuarios a los que se aceptó su start connection
-  Player* connected[2] = { malloc(sizeof(Player)), malloc(sizeof(Player)) };
-  bool acceptedConnection;
+  int timeout_msecs = 500;
+  int ret, i, readedBytes, id;
+  // Player* players[2] = { player_init(), player_init() };
   Card* deck[52];
+  char buffer[256];
+  int playercount = 0;
 
-  while (true) {
-    //setear sockets activos
-    FD_ZERO(&readfds);
-    FD_SET(masterSocket,&readfds);
-    max_sd = masterSocket;
-    for (i = 0; i < MAX_CLIENTS; i++) {
-      sd = clientSockets[i];
-      if (sd > 0)
-        FD_SET(sd,&readfds);
-      if (sd > max_sd)
-        max_sd = sd;
-    }
-
-    //checkear actividad en los sockets
-    actividad = select(max_sd+1, &readfds, NULL, NULL, NULL);
-    if (actividad < 0)
-      printf("Error en el select, al buscar actividad\n");
-
-    //revisar masterSocket
-    if (FD_ISSET(masterSocket, &readfds)) {
-      //nueva conexion
-      new_socket = accept(masterSocket,
-                          (struct sockaddr*)&address,
-                          (socklen_t*)&addressLength);
-
-      //agregarlo a una posicion vacia de clientSockets
-      for (i = 0; i < MAX_CLIENTS; i++) {
-        if (clientSockets[i] == 0) {
-          clientSockets[i] = new_socket;
-          // printf("Agregando cliente a lista de sockets en casilla %i, con valor %i\n",i, new_socket);
-          break;
-        }
-      }
-    }
-
-    //revisar clientes
-    for (i = 0; i < MAX_CLIENTS; i++) {
-      sd = clientSockets[i];
-
-      //NULL si no está conectado
-      Player* currentPlayer = getPlayer(sd, connected);
-
-      if (FD_ISSET(sd, &readfds)) {
-        //escuchar al cliente
-        readedBytes = read( sd , buffer, 256);
-        if (readedBytes == 0){
-            //Un cliente se desconectó
-            getpeername(sd ,
-                        (struct sockaddr*)&address ,
-                        (socklen_t*)&addressLength);
-            printf("Cliente de ip %s se desconectó\n" ,
-                    inet_ntoa(address.sin_addr));
-
-            close(sd);
-            clientSockets[i] = 0;
-        }else{
-          //aca se debe hacer todo el manejo de peticiones por parte del cliente
-          // char* payload = readBuffer(buffer, &id, &payloadSize);
-          char* payload = readBuffer(buffer, &id);
-
-          //convertir a handleRequest
-          if (id == 1) {
-            //Start Connection
-            printf("Cliente solicitó conexión ");
-            acceptedConnection = false;
-            for (i = 0; i < 2; i++) {
-              if (connected[i]->socket == 0) {
-                connected[i]->socket = sd;
-                acceptedConnection = true;
-                break;
+  while(1){
+    ret = poll(fds, 3, timeout_msecs);
+    if (ret > 0) {
+      for (i = 0; i < 3; i++) {
+				if (fds[i].revents) {
+          // Player* currentPlayer = getPlayer(fds[i].fd, players);
+					if (i == 0) {
+						//new connection
+            //WARNING se queda en loop infinito si un tercer jugador ingresa
+						for (int j = 1; j < 3; j++) {
+							if (fds[j].fd < 0) {
+								addr_size = sizeof serverStorage;
+								fds[j].fd = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+								break;
+							}
+						}
+					}else{
+            sleep(1);
+						readedBytes = read(fds[i].fd, buffer, 256);
+						if (readedBytes == 0) {
+							printf("Cliente se ha desconectado\n");
+							fds[i].fd = -1;
+							// WARNING que pasa acá?.
+						}else{
+              char* payload = readBuffer(buffer, &id);
+              if (id == 1) {
+                //Start Connection
+                printf("Cliente solicitó conexión\n");
+                sendMessage(fds[i].fd, "200");
+                //solicitar nickname;
+                sendMessage(fds[i].fd, "300");
+              }else if (id == 24) {
+                //Error Not Implemented
+              }else if (fds[i].fd > 0) {
+                if (id == 4) {
+                  //Return Nickname
+                  printf("Recibido el nickname: %s\n", payload);
+                  strcpy(nicknames[i], payload);
+                  printf("%s, %s\n", nicknames[0], nicknames[1]);
+                  playercount++;
+                  if (playercount == 2) {
+                    opponentFound(nicknames, fds);
+                    // generateDeckOfCards(deck);
+                    // send5Cards(players, deck);
+                  }
+                }else if (id == 13) {
+                  //Return Cards to Change
+                  // changeCards(currentPlayer, payload, deck);
+                }else if (id == 15) {
+                  //Return Bet
+                  //WARNING Falta hacer ésto
+                }
+              }else{
+                //WARNING que hacer?
+                printf("Cliente no conectado enviando data: id:%i | payload: %s\n", id,payload);
               }
-            }
-            if (acceptedConnection) {
-              //id 2, payloadSize 0, payload 0
-              sendMessage(sd, "200");
-              printf("y se aceptó\n");
-            }else{
-              //WARNING que hacer?
-              //lo correcto sería advertirle de que no fue posible conectar
-              printf("pero se rechazó pues ya hay dos jugadores\n");
-            }
-          }else if (id == 24) {
-            //Error Not Implemented
-          }else if (currentPlayer) {
-            if (id == 4) {
-              //Return Nickname
-              currentPlayer->nickname = payload;
-            }else if (id == 13) {
-              //Return Cards to Change
-              changeCards(currentPlayer, payload, deck);
-            }else if (id == 15) {
-              //Return Bet
-              //WARNING Falta hacer ésto
-            }
-          }else{
-            //WARNING que hacer?
-            printf("Cliente no conectado enviando data: id:%i | payload: %s\n", id,payload);
-          }
-
-
-          // printf("Se recibió %s\n",buffer );
-          // send(sd,buffer,strlen(buffer),0);
-        }
+						}
+					}
+				}
       }
     }
   }
-  return 0;
+	return 0;
 }
